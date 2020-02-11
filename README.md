bd4fcbda4396a5c11a70f1755be49b78311164a0
jimmythebrawn <jimmyb@opay.team>
2020-02-11 10:49:42 +0100

    setup project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # styledsyntax-web
+
 React: A simple code parser to migrate from makeStyles (Material-UI) to styled-component syntax
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+
+import React, { useState } from "react";
+
+function App() {
+  const testString = `
+  display: "flex",
+  flexWrap: "nowrap",
+  flexGrow: 1,
+  maxWidth: 315,
+  marginRight: theme.spacing(4),
+`;
+  const [source, setSource] = useState(``);
+
+  const toStyledSyntax = (string: string) => {
+    let newString = string
+      .replace(/([a-z]|(?=[a-z]))([A-Z])/g, "$1-$2")
+      .toLowerCase(); // replace camelCased keys to kebab-case
+    newString = newString.replace(/,\s/g, ";\n"); // replace , with ; on EOL
+    newString = newString.replace(/,\n/g, ";\n"); // replace , with ; on EOL
+    newString = newString.replace(/"/g, ""); // remove quotes (TODO; add exceptions)
+    // eslint-disable-next-line no-template-curly-in-string
+    newString = newString.replace(/(theme.\w+.+\))/g, "${({ theme }) => $1}"); // Refactor theme prop
+
+    if (newString[newString.length - 1] === ",") {
+      newString = newString.replace(/,$/, ";");
+    }
+
+    return newString;
+    // margin: ${({ theme }) => theme.spacing(2, 3)};
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">Styled components syntaxer</header>
+      <main>
+        <div className="panels">
+          <div className="panel panel-left">
+            <label>makeStyles (input)</label>
+            <textarea
+              onChange={e => setSource(e.currentTarget.value)}
+              defaultValue={source}
+            />
+          </div>
+          <div className="panel panel-right">
+            <label>Styled Components (output)</label>
+            <textarea defaultValue={toStyledSyntax(source)} disabled />
+          </div>
+        </div>
+        {/* <pre>
+          display: "flex", flexWrap: "nowrap", flexGrow: 1, maxWidth: 315,
+          marginRight: theme.spacing(4),
+        </pre> */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
